ce1b8934f20eb6e2b640f046c7913da809d37bbe
FadedGuy <42686178+Kevinazo042444@users.noreply.github.com>
2019-12-02 17:01:44 -0600

    VersionBien

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,7 +13,7 @@ export class HomePage {
   constructor(private modalCtrl: ModalController) {
     setTimeout(() =>{
       this.lockApp();
-    },2000);
+    },20000);
   }
   async lockApp(){
     const modal = await this.modalCtrl.create({
@@ -28,31 +28,3 @@ export class HomePage {
 
   }
 }
-
-/*
-this.fingerprintAuth.isAvailable()
-  .then((result)=> {
-    if(result.isAvailable){
-      // it is available
-
-      this.androidFingerprintAuth.encrypt({ clientId: 'myAppName', username: 'myUsername', password: 'myPassword' })
-        .then(result => {
-           if (result.withFingerprint) {
-               console.log('Successfully encrypted credentials.');
-               console.log('Encrypted credentials: ' + result.token);
-           } else if (result.withBackup) {
-             console.log('Successfully authenticated with backup password!');
-           } else console.log('Didn\'t authenticate!');
-        })
-        .catch(error => {
-           if (error === this.androidFingerprintAuth.ERRORS.FINGERPRINT_CANCELLED) {
-             console.log('Fingerprint authentication cancelled');
-           } else console.error(error)
-        });
-
-    } else {
-      // fingerprint auth isn't available
-    }
-  })
-  .catch(error => console.error(error));
-*/
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -18,6 +18,8 @@ export class LoginPage implements OnInit {
   }
 
 login() {
+  this.router.navigateByUrl('/home');
+  /*
   this.faio.show({
 
   }).then(() => {
@@ -30,6 +32,6 @@ login() {
   })
   .catch((error: any) => {
     console.log('err: ', error);
-  });
+  });*/
 }
 }
