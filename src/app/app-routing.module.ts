e34e48e4de820be57e288725d560312fa39d9189
desparza13 <desparzaespinosa@gmail.com>
2019-12-02 09:23:54 -0600

    Paginas añadidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'directorio',
+    loadChildren: () => import('./pages/directorio/directorio.module').then( m => m.DirectorioPageModule)
+  },
+  {
+    path: 'galeria',
+    loadChildren: () => import('./pages/galeria/galeria.module').then( m => m.GaleriaPageModule)
+  },
+  {
+    path: 'notas',
+    loadChildren: () => import('./pages/notas/notas.module').then( m => m.NotasPageModule)
+  },
 ];
 
 @NgModule({
